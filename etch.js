2b1e43367fe19ded4681f8e662f33b905ac2a331
DarthKormit <space.marine@hotmail.co.nz>
2022-11-10 01:05:52 +1300

    gridDivs now change colour on mouse click and mouse enter

--- etch.js
+++ etch.js
@@ -1,17 +1,41 @@
 const grid = document.getElementById("div-grid");
+const gridDivs = document.getElementsByClassName("grid-div");
 
 function createDiv(divNumber) {
-    var loops = divNumber;
-    for (let i = 0; i < loops; i++) {
-        let newDiv = document.createElement("div");
-        const newContent = document.createTextNode("Hi there and greetings!");
-        newDiv.appendChild(newContent)
-        newDiv.className = "grid-div"
-        const currentDiv = document.getElementById("div1");
-        // document.body.insertBefore(newDiv, currentDiv);
-        grid.insertBefore(newDiv, currentDiv)
-    }
+  var loops = divNumber;
+  for (let i = 0; i < loops; i++) {
+    let newDiv = document.createElement("div");
+    const newContent = document.createTextNode("");
+    newDiv.appendChild(newContent);
+    newDiv.className = "grid-div";
+    const currentDiv = document.getElementById("div1");
+    // document.body.insertBefore(newDiv, currentDiv);
+    grid.insertBefore(newDiv, currentDiv);
+  }
 }
 
+function createListeners() {
+  let isClicked = false;
+  grid.addEventListener("mousedown", () => {
+    isClicked = true;
+  });
+  grid.addEventListener("mouseup", () => {
+    isClicked = false;
+  });
+
+  Array.from(gridDivs).forEach((element) => {
+    element.addEventListener("mousedown", () => {
+      element.style.backgroundColor = "purple";
+      console.log("data-wow value is: ");
+    });
+
+    element.addEventListener("mouseenter", () => {
+      if (isClicked == true) {
+        element.style.backgroundColor = "purple";
+      }
+    });
+  });
+}
 
 createDiv(9);
+createListeners();
