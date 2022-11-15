3b8df19bd29d29bcbc52bedb0a38b6107db2e5c0
DarthKormit <space.marine@hotmail.co.nz>
2022-11-16 00:21:12 +1300

    added the ability to choose a colour, made it so that the canvas cannot be dragged by the browser through css

--- etch.js
+++ etch.js
@@ -2,12 +2,15 @@ const grid = document.getElementById("div-grid");
 const gridDivs = document.getElementsByClassName("grid-div");
 const canvasSize = document.getElementById("canvas-size");
 const resizeCanvas = document.getElementById("resize-canvas");
+const clearCanvas = document.getElementById("clear-canvas");
+const colourPicker = document.getElementById("colour-picker");
 
 var output = document.getElementById("demo");
 canvasSize.value = 9;
 output.innerHTML = canvasSize.value;
 
 let isClicked = false;
+let chosenColour = "black";
 
 canvasSize.oninput = function () {
   output.innerHTML = this.value;
@@ -19,6 +22,12 @@ function deleteChild() {
   }
 }
 
+function clearGridDivs() {
+  Array.from(gridDivs).forEach((element) => {
+    element.style.backgroundColor = "antiquewhite";
+  });
+}
+
 function createDiv(divNumber) {
   deleteChild();
   var loops = divNumber * divNumber;
@@ -26,7 +35,6 @@ function createDiv(divNumber) {
     let newDiv = document.createElement("div");
     newDiv.className = "grid-div";
     grid.insertAdjacentElement("beforeend", newDiv);
-    
   }
   createListeners();
   grid.style.setProperty("--columnnumber", divNumber);
@@ -44,28 +52,24 @@ function createGridListeners() {
 
 function createListeners() {
   Array.from(gridDivs).forEach((element) => {
-    element.addEventListener("click", () => {
-      element.style.backgroundColor = "purple";
+    element.addEventListener("mousedown", () => {
+      element.style.backgroundColor = chosenColour;
     });
 
     element.addEventListener("mouseenter", () => {
       if (isClicked == true) {
-        element.style.backgroundColor = "purple";
+        element.style.backgroundColor = chosenColour;
       }
     });
   });
 }
 
-function clearGridDivs(params) {
-  Array.from(gridDivs).forEach((element) => {
-    element.addEventListener("click", () => {
-      element.style.backgroundColor = "white";
-    });
-
-    );
+function watchColorPicker(event) {
+  chosenColour = event.target.value;
 }
 
-
+colourPicker.addEventListener("change", watchColorPicker, false);
+clearCanvas.addEventListener("click", () => clearGridDivs());
 resizeCanvas.addEventListener("click", () => createDiv(canvasSize.value));
 
 createGridListeners();
